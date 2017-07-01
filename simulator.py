baa58117cd83972de0f83821685d5eeca6fabfdf
Gary Roberts <g_roberts@u.pacific.edu>
2017-07-01 12:53:08 -0700

    Temp argparse param for testing

--- simulator.py
+++ simulator.py
@@ -1,12 +1,16 @@
 ''' Simulator.py '''
 from scheduler import InOrder
 from disk import Disk
+from graph_tool.all import *
 import argparse
 
 def main():
     ''' Parse CLI args and invoke simulator '''
-    #parser = argparse.ArgumentParser()
-    #args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    graph_g = parser.add_mutually_exclusive_group()
+    graph_g.add_argument('--random', help='Random graph generation', action='store_true')
+    graph_g.add_argument('--static', metavar='S', help='Use static graph', choices=['331', 391])
+    args = parser.parse_args()
 
     s = InOrder()
 
