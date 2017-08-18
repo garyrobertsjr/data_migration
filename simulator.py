da6f73653bd671801d78b140efdfef70764b0f43
Gary Roberts <g_roberts@u.pacific.edu>
2017-08-18 14:40:37 -0700

    Graph Import/Exporting

--- simulator.py
+++ simulator.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 ''' Simulator.py '''
-from scheduler import InOrder, Greedy, Color
+from scheduler import InOrder, Greedy, Split_CV
 from disk import Disk
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -11,13 +11,13 @@ import argparse
 def main():
     ''' Parse CLI args and invoke simulator '''
     parser = argparse.ArgumentParser()
-    parser.add_argument('scheduler', help='Specifiy scheduler algorithm', choices=['inorder', 'random', 'greedy', 'color'])
+    parser.add_argument('scheduler', help='Specifiy scheduler algorithm', choices=['inorder', 'random', 'greedy', 'split'])
     cv_g = parser.add_mutually_exclusive_group()
     cv_g.add_argument('--static_cv', help='Specifiy cv', type=int)
     cv_g.add_argument('--rand_cv', help='Specifiy max value for random cv', type=int)
     graph_g = parser.add_mutually_exclusive_group()
-    graph_g.add_argument('--random_edges', help='Random graph generation', type=int)
-    graph_g.add_argument('--demo', metavar='S', help='Use demo graph', choices=['331', '391'])
+    graph_g.add_argument('--random', help='Random graph generation', type=int)
+    graph_g.add_argument('--file', metavar='F', help='Import graph from pickle', type=argparse.FileType('rb'))
     args = parser.parse_args()
 
     if args.scheduler == 'inorder':
@@ -27,16 +27,16 @@ def main():
         return
     elif args.scheduler == 'greedy':
         sched = Greedy()
-    elif args.scheduler == 'color':
-        sched = Color()
+    elif args.scheduler == 'split':
+        sched = Split_CV()
 
     disks = []
 
     g = nx.MultiGraph()
 
-    if(args.random_edges):
+    if(args.random):
         ''' Populate disk list '''
-        for i in range(args.random_edges):
+        for i in range(args.random):
             if args.rand_cv:
                 disks.append(Disk(random.randint(1,args.rand_cv),0))
             elif args.static_cv:
@@ -47,13 +47,18 @@ def main():
         g.add_nodes_from(disks)
 
         ''' Random graph generation '''
-        for s,t in zip(randint(0,args.random_edges,args.random_edges*5), \
-		                randint(0,args.random_edges,args.random_edges*5)):
+        for s,t in zip(randint(0,args.random,args.random*5), \
+		                randint(0,args.random,args.random)):
             g.add_edge(disks[s],disks[t])
 
-    elif(args.static):
-        pass
+        # Write graph pickle to file. 
+        # TODO: Naming schema
+        nx.write_gpickle(g, "output.gpickle")
 
+    elif(args.file):
+        # Import graph pickle
+        g = nx.read_gpickle(args.file)
+    
     nx.draw_random(g)
     plt.savefig("graph.png")
 
